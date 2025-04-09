bf3d49b81f88e2f2a4666aafe1e8310c60577748
fassabilf <fassabilf@gmail.com>
2025-04-09 09:50:25 +0700

    chore: initialize project for recruitment-service

---
+++ src/test/java/id/ac/ui/cs/advprog/b13/hiringgo/recruitment/RecruitmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b13.hiringgo.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
