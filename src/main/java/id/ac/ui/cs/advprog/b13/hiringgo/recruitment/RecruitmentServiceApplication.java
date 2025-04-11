b91e7be444da4d35f3b7c2f36ec47461c327d5ff
Ramy Ardya <161923530+UltraRamy@users.noreply.github.com>
2025-04-11 14:50:18 +0700

    TestDB

--- src/main/java/id/ac/ui/cs/advprog/b13/hiringgo/recruitment/RecruitmentServiceApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/b13/hiringgo/recruitment/RecruitmentServiceApplication.java
@@ -2,6 +2,11 @@ package id.ac.ui.cs.advprog.b13.hiringgo.recruitment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class RecruitmentServiceApplication {
@@ -10,4 +15,15 @@ public class RecruitmentServiceApplication {
         SpringApplication.run(RecruitmentServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner testDbConnection(DataSource dataSource) {
+        return args -> {
+            try (Connection conn = dataSource.getConnection()) {
+                System.out.println("✅ Connected to DB: " + conn.getMetaData().getURL());
+            } catch (Exception e) {
+                System.out.println("❌ Failed to connect to DB");
+                e.printStackTrace();
+            }
+        };
+    }
 }
