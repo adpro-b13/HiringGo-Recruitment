462dd539751e2480bbaf65f95681aa74b4a016d2
lhakiim <luqmanulhakim2021@gmail.com>
2025-05-02 20:15:11 +0700

    [RED] add mendaftar lowongan serviceimpl

---
+++ src/test/java/id/ac/ui/cs/advprog/b13/hiringgo/recruitment/service/PendaftaranLowonganServiceImpl.java
@@ -0,0 +1,55 @@
+package id.ac.ui.cs.advprog.b13.hiringgo.recruitment.service;
+
+import id.ac.ui.cs.advprog.b13.hiringgo.recruitment.model.PendaftaranLowongan;
+import id.ac.ui.cs.advprog.b13.hiringgo.recruitment.repository.PendaftaranLowonganRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PendaftaranLowonganServiceImplTest {
+
+    @Mock
+    private PendaftaranLowonganRepository repository;
+
+    @InjectMocks
+    private PendaftaranLowonganServiceImpl service;
+
+    @Test
+    void testSave() {
+
+        PendaftaranLowongan pendaftaran = new PendaftaranLowongan();
+
+        when(repository.save(pendaftaran)).thenReturn(pendaftaran);
+
+        PendaftaranLowongan saved = service.save(pendaftaran);
+
+        assertNotNull(saved);
+        assertEquals(pendaftaran, saved);
+    }
+
+    @Test
+    void testFindByLowonganId() {
+
+        Long id = 1L;
+        PendaftaranLowongan pendaftaran = new PendaftaranLowongan();
+
+        pendaftaran.setId(1L);
+
+        List<PendaftaranLowongan> list = List.of(pendaftaran);
+
+        when(repository.findByLowonganId(id)).thenReturn(list);
+
+        List<PendaftaranLowongan> result = service.findByLowongan(id);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(id, result.get(0).getId());
+    }
+}
